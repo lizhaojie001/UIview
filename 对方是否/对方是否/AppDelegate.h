19fac489f37c3b7ea603a8e0ac2673ca4d75b21d
lizhaojie <252487266@qq.com>
2015-12-31 18:04:55 +0800

    fds

---
+++ 对方是否/对方是否/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  对方是否
+//
+//  Created by tareba on 15/12/31.
+//  Copyright © 2015年 tanada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 对方是否/对方是否/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  对方是否
+//
+//  Created by tareba on 15/12/31.
+//  Copyright © 2015年 tanada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
